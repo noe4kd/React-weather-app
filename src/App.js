9a90a4b54e9c767f12a5d9d039e02f14095266c4
Noe4kd <noelia4kd@gmail.com>
2022-10-19 12:39:51 +0200

    rel="noreferrer" added to links

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ function App() {
     <div className="App">
       <h1>Weather app</h1>
       <footer>
-        <a href="https://github.com/noe4kd/React-weather-app" target="_blank">
+        <a
+          href="https://github.com/noe4kd/React-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           Open-sourced
         </a>{" "}
         coded by{" "}
-        <a href="https://portfolioproject-noe4kd.netlify.app/" target="_blank">
+        <a
+          href="https://portfolioproject-noe4kd.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Noelia 4kd{" "}
         </a>
       </footer>
