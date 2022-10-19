898d14033a72d5e91f29743872693005f5fc8152
Noe4kd <noelia4kd@gmail.com>
2022-10-19 12:38:20 +0200

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        <a href="https://github.com/noe4kd/React-weather-app" target="_blank">
+          Open-sourced
+        </a>{" "}
+        coded by{" "}
+        <a href="https://portfolioproject-noe4kd.netlify.app/" target="_blank">
+          Noelia 4kd{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
