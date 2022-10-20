a1bdc9efba8fd1b69e23a6ea8755096389efeae8
Noe4kd <noelia4kd@gmail.com>
2022-10-20 09:37:51 +0200

    added boostrap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        <a
-          href="https://github.com/noe4kd/React-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced
-        </a>{" "}
-        coded by{" "}
-        <a
-          href="https://portfolioproject-noe4kd.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Noelia 4kd{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          <a
+            href="https://github.com/noe4kd/React-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced
+          </a>{" "}
+          coded by{" "}
+          <a
+            href="https://portfolioproject-noe4kd.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Noelia 4kd{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
