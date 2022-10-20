70f557eadf956311f084aaf86e1ce9feedc2d0a5
Noe4kd <noelia4kd@gmail.com>
2022-10-20 14:17:51 +0200

    lay out weather app created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app</h1>
+        <Weather />
         <footer>
           <a
             href="https://github.com/noe4kd/React-weather-app"
