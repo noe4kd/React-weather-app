4cb4a7c64706a2582c6e6bb092ad5053d398b3e1
Noe4kd <noelia4kd@gmail.com>
2022-10-25 11:09:31 +0200

    date updated, CSS border added

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -11,6 +11,18 @@ export default function FormattedDate(props) {
     "Saturday",
   ];
   let day = days[props.date.getDay()];
-
-  return <div>{day}</div>;
+  let hours = props.date.getHours();
+  let minutes = props.date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return (
+    <div>
+      {day} {}
+      {hours}:{minutes}
+    </div>
+  );
 }
