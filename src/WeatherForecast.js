c767a4480cc5136c379897bb9f804ce6fa6fc41c
Noe4kd <noelia4kd@gmail.com>
2022-11-03 12:36:56 +0100

    forecast design corrected

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,46 +15,6 @@ export default function WeatherForecast() {
             </spann>
           </div>
         </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu </div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
-          <div className="WeatherForcast-temperature">
-            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
-            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
-              10°
-            </spann>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu </div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
-          <div className="WeatherForcast-temperature">
-            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
-            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
-              10°
-            </spann>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu </div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
-          <div className="WeatherForcast-temperature">
-            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
-            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
-              10°
-            </spann>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu </div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
-          <div className="WeatherForcast-temperature">
-            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
-            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
-              10°
-            </spann>
-          </div>
-        </div>
       </div>
     </div>
   );
