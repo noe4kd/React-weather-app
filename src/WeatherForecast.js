ba435d9c795020f9d9bc67a3cac0a35fd7259663
Noe4kd <noelia4kd@gmail.com>
2022-11-03 12:34:47 +0100

    weather forecast designed CSS

---
+++ src/WeatherForecast.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu </div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
+          <div className="WeatherForcast-temperature">
+            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
+            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
+              10°
+            </spann>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu </div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
+          <div className="WeatherForcast-temperature">
+            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
+            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
+              10°
+            </spann>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu </div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
+          <div className="WeatherForcast-temperature">
+            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
+            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
+              10°
+            </spann>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu </div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
+          <div className="WeatherForcast-temperature">
+            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
+            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
+              10°
+            </spann>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu </div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"></img>{" "}
+          <div className="WeatherForcast-temperature">
+            <spann className="WeatherForecast-temperature-max me-1">19°</spann>
+            <spann className="WeatherForecast-temperature-min opacity-75 ms-1">
+              10°
+            </spann>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
